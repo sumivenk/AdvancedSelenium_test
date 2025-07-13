b0b297292fd10e1b3649a92e1159b8407bf9631e
sumivenk <42695914+sumivenk@users.noreply.github.com>
2025-07-13 19:30:19 +0530

    Update CreateCampaignTest.java

--- AdvancedSelenium/src/test/java/campaigns/CreateCampaignTest.java
+++ AdvancedSelenium/src/test/java/campaigns/CreateCampaignTest.java
@@ -43,6 +43,7 @@ public class CreateCampaignTest extends Base{
 	public void createCampaignTest() throws Throwable {
 	//I am pulling Pull1
 		//Push back
+		//Pull2
 		
 //		Login to vtiger application->mouseOverOn more Link->click on campaigns->click on create campaign lookup image->Enter campaignName->
 //		click on save Btn->verify whether the campaign is created in campaign Information page and Logout from the application. 
